2e70606f3e5e44acc1e293d915ac614b05a8d94b
Achraf Kassioui <achrafkassioui@gmail.com>
2019-03-22 22:11:40 +0000

    Update filtreur.js

--- filtreur.js
+++ filtreur.js
@@ -2,7 +2,7 @@
 //
 // Filtreur.js
 // 0.9 Beta
-// 14 March 2019
+// 22 March 2019
 //
 // www.achrafkassioui.com/filtreur/
 //
@@ -41,7 +41,11 @@
     ////////////////////////////////////////////////////////////////////////
 
     var supports = !!document.addEventListener && !!document.querySelector && !!Array.prototype.filter && !!Array.prototype.includes;
-    var filtreur = {};
+    var filtreur = {
+        running: false,
+        keyboard: false,
+        keycodes: {}
+    };
 
     ////////////////////////////////////////////////////////////////////////
     //
@@ -52,11 +56,20 @@
     filtreur.all = 'all';
     filtreur.filtered_out = 'filtered_out';
     filtreur.filtered_in = 'filtered_in';
-    filtreur.control_selected = 'selected';
+    filtreur.selected_filter = 'selected_filter';
     filtreur.toggle = true;
     filtreur.escape = true;
-    filtreur.keyboard = false;
-    filtreur.keycodes = {};
+
+    ////////////////////////////////////////////////////////////////////////
+    //
+    // HTML API
+    //
+    ////////////////////////////////////////////////////////////////////////
+
+    var data_attribute_filter = 'data-filter';
+    var data_attribute_filter_in = 'data-filter-in';
+    var data_attribute_collection = 'data-collection';
+    var data_attribute_keycode = 'data-filter-keycode';
 
     ////////////////////////////////////////////////////////////////////////
     //
@@ -74,56 +87,59 @@
 
         filtreur.collection = null;
 
-        updateUI(data);
-
         if (filtreur.callback_end) filtreur.callback_end();
+
+        updateUI(data);
     }
 
     function updateUI(data) {
         getControls(data.collection).filter(function (control) {
-            var filters = control.getAttribute('data-filter').split(' ');
+            var filters = control.getAttribute(data_attribute_filter).split(' ');
+            // add fail safe to deal with buttons without a data-filter
             var hasCategory = (filters.indexOf(data.filter) > -1);
             var isSelectBox = control.parentElement.selectedIndex;
 
             if (hasCategory) {
                 if (isSelectBox != undefined) control.setAttribute('selected', 'true');
-                else control.classList.add(filtreur.control_selected);
+                else control.classList.add(filtreur.selected_filter);
             } else {
                 if (isSelectBox != undefined) control.removeAttribute('selected');
-                control.classList.remove(filtreur.control_selected);
+                control.classList.remove(filtreur.selected_filter);
             }
         });
     }
 
     function getItems(collection){
-        var items = Array.prototype.slice.call(document.querySelectorAll('[data-collection=' + collection +'] *'));
+        var items = Array.prototype.slice.call(document.querySelectorAll('[' + data_attribute_collection + '=' + collection +'] *'));
         return items;
     }
 
     function getControls(collection){
-        var controls = Array.prototype.slice.call(document.querySelectorAll('[data-filter-in=' + collection +']'));
+        var controls = Array.prototype.slice.call(document.querySelectorAll('[' + data_attribute_filter_in + '=' + collection +']'));
         return controls;
     }
 
     function setupKeyboardShortcuts(){
-        var controls = Array.prototype.slice.call(document.querySelectorAll('[data-filter-keycode]'));
+        var controls = Array.prototype.slice.call(document.querySelectorAll('[' + data_attribute_keycode + ']'));
         if(!controls) return;
 
         filtreur.keyboard = true;
 
         controls.filter(function(control){
-            var keycode = control.getAttribute('data-filter-keycode');
+            var keycode = control.getAttribute(data_attribute_keycode);
 
-            var filter = control.getAttribute('data-filter');
+            var filter = control.getAttribute(data_attribute_filter);
+            // Move the failsafe check below into the sanitize function
             if(!filter){
                 var obj = {item: control};
-                return console.warn('Filtreur:', obj.item, ' has no "data-filter" attribute');
+                return console.warn('Filtreur:', obj.item, ' has no ' + JSON.stringify(data_attribute_filter) + ' attribute');
             }
 
-            var collection = control.getAttribute('data-filter-in');
+            var collection = control.getAttribute(data_attribute_filter_in);
+            // Move the failsafe check below into the sanitize function
             if(!collection){
                 var obj = {item: control};
-                return console.warn('Filtreur:', obj.item, ' has no "data-filter-in" attribute');
+                return console.warn('Filtreur:', obj.item, ' has no ' + JSON.stringify(data_attribute_filter_in) + ' attribute');
             }
 
             filtreur.keycodes[keycode] = 1;
@@ -139,15 +155,15 @@
     filtreur.filter = function (data) {
         if (data.filter === filtreur.all && filtreur.current === filtreur.all) return;
 
-        if (filtreur.toggle && data.filter === filtreur.current) return unFilter(data);
+        if (filtreur.toggle && data.filter === filtreur.current && data.collection === filtreur.collection) return unFilter(data);
 
         filtreur.current = data.filter;
 
         if (filtreur.current === filtreur.all) return unFilter(data);
 
         getItems(data.collection).filter(function (item) {
-            if (!(item.hasAttribute('data-filter') && !item.hasAttribute('data-filter-in'))) return;
-            var filters = item.getAttribute('data-filter').split(' ');
+            if (!(item.hasAttribute(data_attribute_filter) && !item.hasAttribute(data_attribute_filter_in))) return;
+            var filters = item.getAttribute(data_attribute_filter).split(' ');
             var hasCategory = (filters.indexOf(data.filter) > -1);
 
             if (!hasCategory) {
@@ -161,10 +177,42 @@
 
         filtreur.collection = data.collection;
 
-        updateUI(data);
-
         if (filtreur.callback_start) filtreur.callback_start();
+
+        updateUI(data);
     }
+
+    filtreur.sanitize = function(){
+        // if an element has a data-filter-in, check if it has data-filter
+        // if an element has a data-filter-keycode, check if it has a data-filter and a data-filter-in
+        // if there is no collection, warn
+        // if there is a collection, check if it has elements that can filtered (i.e. with a data-filter only)
+    };
+
+    filtreur.stop = function(){
+        if(filtreur.running === false) return;
+
+        document.removeEventListener('click', eventHandler, false);
+        document.removeEventListener('change', eventHandler, false);
+        document.removeEventListener('keydown', eventHandler, false);
+
+        filtreur.running = false;
+        return console.log('Filtreur has been stopped');
+    };
+
+    filtreur.start = function(){
+        if (!supports) return console.warn('Filtreur.js is not supported on this browser');
+        filtreur.stop();
+
+        setupKeyboardShortcuts();
+
+        document.addEventListener('click', eventHandler, false);
+        document.addEventListener('change', eventHandler, false);
+        document.addEventListener('keydown', eventHandler, false);
+
+        filtreur.running = true;
+        return console.log('Filtreur is running');
+    };
 
     ////////////////////////////////////////////////////////////////////////
     //
@@ -174,25 +222,25 @@
 
     function eventHandler(e) {
         if (e.type === 'click') {
-            var isControl = e.target.hasAttribute('data-filter-in');
-            var hasFilter = e.target.hasAttribute('data-filter');
+            var isControl = e.target.hasAttribute(data_attribute_filter_in);
+            var hasFilter = e.target.hasAttribute(data_attribute_filter);
             if (!isControl || !hasFilter) return;
 
             filtreur.filter({
-                filter: e.target.dataset.filter,
-                collection: e.target.dataset.filterIn
+                filter: e.target.getAttribute(data_attribute_filter),
+                collection: e.target.getAttribute(data_attribute_filter_in)
             });
         }
 
         if (e.type === 'change') {
             var isSelectBox = e.target.selectedIndex;
-            var isControl = e.target.options[e.target.selectedIndex].hasAttribute('data-filter-in');
-            var hasFilter = e.target.options[e.target.selectedIndex].hasAttribute('data-filter');
+            var isControl = e.target.options[e.target.selectedIndex].hasAttribute(data_attribute_filter_in);
+            var hasFilter = e.target.options[e.target.selectedIndex].hasAttribute(data_attribute_filter);
             if (!((isSelectBox != undefined) && isControl && hasFilter)) return;
 
             filtreur.filter({
-                filter: e.target.options[e.target.selectedIndex].dataset.filter,
-                collection: e.target.options[e.target.selectedIndex].dataset.filterIn
+                filter: e.target.options[e.target.selectedIndex].getAttribute(data_attribute_filter),
+                collection: e.target.options[e.target.selectedIndex].getAttribute(data_attribute_filter_in)
             });
         }
 
@@ -200,11 +248,11 @@
             var modifiers = e.ctrlKey || e.shiftKey || e.altKey;
 
             if (filtreur.keyboard && filtreur.keycodes[e.keyCode] && !modifiers){
-                var button = document.querySelector('[data-filter-keycode="' + e.keyCode + '"]');
+                var button = document.querySelector('[' + data_attribute_keycode + '="' + e.keyCode + '"]');
 
                 filtreur.filter({
-                    filter: button.getAttribute('data-filter'),
-                    collection: button.getAttribute('data-filter-in')
+                    filter: button.getAttribute(data_attribute_filter),
+                    collection: button.getAttribute(data_attribute_filter_in)
                 });
             }
 
@@ -222,15 +270,7 @@
     //
     ////////////////////////////////////////////////////////////////////////
 
-    if (!supports) return console.warn('Filtreur.js is not supported on this browser');
-
-    setupKeyboardShortcuts();
-
-    document.addEventListener('click', eventHandler, false);
-    document.addEventListener('change', eventHandler, false);
-    document.addEventListener('keydown', eventHandler, false);
-
-    console.log('Filtreur is running');
+    filtreur.start();
 
     return filtreur;
 });
@@ -239,8 +279,10 @@
 
 To do
 
+- Write a sanitize function that runs on startup
 - Should controls accept multiple filters?
 - Similar control keycodes should work on all collections.
-- filtreur.live option. I false, store the controls and items in a variable on startup.
+- filtreur.live setting. If false, store the all controls and collections in a variable on startup.
+- Bug: pressing escape when an option in a select box has been selected with the keyboard does not select the all option
 
 */
